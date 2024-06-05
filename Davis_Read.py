a10383051766df82f042e1eae7cf2a0fab9feb19
Raymond <63129961+Mraymon5@users.noreply.github.com>
2024-06-05 16:14:46 -0400

    Update Davis_Read.py
    Cleaning, and added some plotting capacity

--- Davis_Read.py
+++ Davis_Read.py
@@ -2,28 +2,62 @@
 import os
 import pandas as pd
 import numpy as np
-import time
 import warnings
 import matplotlib.pyplot as plt
 
 #%%
-def Davis_Read(folder=None, 
-               keyword=".txt", 
-               file=None, 
-               intervalCutoff=50,
-               burstCutoff=500,
-               minBurstLength=1,
-               output_folder=None, 
-               ili_folder=None, 
-               title=None, 
-               plot_raw_interval=False, 
-               plot_interval=False, 
-               plot_cumulative=False, 
-               timer=False, 
-               stjohn_file=None):
+def Davis_Read(folder=None, #Path to a folder containing the .ms8.txt files to be processed
+               keyword=".MS8.txt", #A search term to identify files in that folder
+               file=None, #A specific single file to be processed
+               intervalCutoff=50, #The minimum ILI to be considered a lick; anything less is treated as double-contact
+               burstCutoff=500, #The maximum ILI allowed within a single burst; anything greater is treated as a pause
+               minBurstLength=1, #The minimum number of licks to constitute a burst; smaller clusters aren't counted as bursts
+               output_folder=None, #The path to a folder that trial data will be saved to
+               ili_folder=None, #The path to a folder that ILI data will be saved to
+               title=None, #A string used to name outputs, optional /not implemented
+               plot_raw_interval=False, #plot a histogram of raw ILIs
+               plot_interval=False, #plot a histogram of filtered ILIs /not implemented
+               plot_cumulative=False, #plot cumulative licks over the session /not implemented
+               legacy_file=None): #the path to a legacy file containing the ILI info extracted from a .ms8 file, requires that the .ms8 file be supplied to 'file' arguement. You probably don't need this.
+    """ 
+    Davis_Read is used to process .ms8.txt files, returning data in csv form, and includes some data cleaning and microsctructure analysis steps.
+    
+    Parameters
+    ----------
+    folder : str
+        The path to a folder containing multiple .ms8.txt files to be processed at once. Incompatible with 'file', and 'legacy_file'.
+    keyword : str, optional
+        A string common to the file names of the target files.
+    file : str
+        The path to a single file to be processed. Incompatible with 'folder', and required for 'legacy_file'; if using 'legacy_file', should target a .ms8 file rather than a .ms8.txt file.
+    intervalCutoff : int, optional
+        The minimum ILI (in mS) to be considered a lick; anything less is treated as double-contact .
+    burstCutoff : int, optional
+        The maximum ILI (in mS) allowed within a single burst; anything greater is treated as a pause.
+    minBurstLength : int, optional
+        The minimum number of licks to constitute a burst; smaller clusters aren't counted as bursts.
+    output_folder : str, optional
+        The path to a folder that trial data will be saved to.
+    ili_folder : str, optional
+        The path to a folder that ILI data will be saved to.
+    title : str, optional
+        A string used to name outputs, optional /not implemented.
+    plot_raw_interval : bool, optional
+        Plot a histogram of raw ILIs /not implemented.
+    plot_interval : bool, optional
+        Plot a histogram of filtered ILIs /not implemented.
+    plot_cumulative : bool, optional
+        Plot cumulative licks over the session /not implemented.
+    legacy_file : str, optional
+        The path to a legacy file containing the ILI info extracted from a .ms8 file, requires that the .ms8 file be supplied to 'file' arguement. You probably don't need this.
+    
+    Returns
+    -------
+    pass
+    """
     if 0:
         folder="/home/ramartin/Documents/Code/Python/Davis/Test_Data"
-        keyword=".txt";
+        keyword=".MS8.txt";
         file=None;
         intervalCutoff=50;
         burstCutoff=500;
@@ -34,17 +68,18 @@ def Davis_Read(folder=None,
         plot_raw_interval=False;
         plot_interval=False;
         plot_cumulative=False;
-        timer=False;
-        stjohn_file=None;
+        legacy_file=None;
     
-    if timer:
-        start_time = time.time()
-
     if folder is None and file is None:
-        raise ValueError("You must specify a file or a folder")
+        raise ValueError("You must specify a 'file' or a 'folder'")
+        
+    if legacy_file is not None and file is None:
+        raise ValueError("If processing a legacy file, 'file' must be a corrosponding .ms8")
     
     if folder:
         file_names = [os.path.join(folder, f) for f in os.listdir(folder) if keyword in f]
+        if len(file_names)==0:
+                raise ValueError("No files in 'folder' match 'keyword'")
         directory = pd.DataFrame({'Files': [f for f in os.listdir(folder) if keyword in f], 'Date': None, 'Animal': None})
     elif file:
         file_names = [file]
@@ -58,16 +93,17 @@ def Davis_Read(folder=None,
         directory.at[i, 'Date'] = f"{directory.at[i, 'Files'][:2]}/{directory.at[i, 'Files'][2:4]}"
         directory.at[i, 'Animal'] = directory.at[i, 'Files'][4:20]
 
-
+    returnedOutput = {}
+    
     for fileN, fileIs in enumerate(file_names):#fileN = 0;fileIs = file_names[fileN]
         lick_export = []
 
         version = pd.read_csv(fileIs, header=None, sep=None, engine='python', nrows=1)
         
-        if stjohn_file:
-            temp_lat = pd.read_csv(stjohn_file, skiprows=3, nrows=1, delim_whitespace=True).values.T
+        if legacy_file:
+            temp_lat = pd.read_csv(legacy_file, skiprows=3, nrows=1, delim_whitespace=True).values.T
             temp_lat = float(temp_lat[5] if temp_lat[6] == "s" else temp_lat[5]) * 1000
-            licks_dav = pd.read_csv(stjohn_file, skiprows=9).values.T;
+            licks_dav = pd.read_csv(legacy_file, skiprows=9).values.T;
             meta = pd.read_csv(file, skiprows=5, nrows=2, sep=",", index_col=0, skipinitialspace=1)
             meta_data = pd.read_csv(file, skiprows=8, nrows=int(meta.at[2, 1]), sep=",", names=["PRES", "TUBE", "CONCENTRATION", "SOLUTION", "IPI", "LENGTH", "LICKS"], skipinitialspace=1)
             meta_data['Latency'] = temp_lat
@@ -117,7 +153,7 @@ def Davis_Read(folder=None,
             #This Loop Filters licks for double contacts, and keeps track of bursts      
             for lickN in range(1, len(lick_data)):
                 lick_data.loc[lickN,'RawInterval'] = lick_data.loc[lickN,'RawTime']-lick_data.loc[lickN-1,'RawTime']
-                if lick_data.at[lickN, 'RawTime'] - lick_data.at[lickN-1, 'FilteredTime'] > intervalCutoff:
+                if lick_data.at[lickN, 'RawTime'] - lick_data.at[lickN-1, 'FilteredTime'] >= intervalCutoff:
                     lick_data.at[lickN, 'FilteredTime'] = lick_data.at[lickN, 'RawTime']
                     lick_data.loc[range(lickN,len(lick_data)), 'LickTotal'] += 1
                     if lick_data.at[lickN, 'FilteredTime'] - lick_data.at[lickN-1, 'FilteredTime'] > burstCutoff or lickN == 1:
@@ -155,8 +191,8 @@ def Davis_Read(folder=None,
 
             #Compute MPI and Efficiency, and deal with no-lick trials
             if lick_data['LickTotal'].max() > 0:
-                MPI = lick_data['FilteredInterval'][(lick_data['FilteredInterval'] > intervalCutoff) & (lick_data['FilteredInterval'] <= 160)].mean()
-                Efficiency = len(lick_data['FilteredInterval'][(lick_data['FilteredInterval'] > intervalCutoff) & (lick_data['FilteredInterval'] <= 160)]) / (lick_data['LickTotal'].max())
+                MPI = lick_data['FilteredInterval'][(lick_data['FilteredInterval'] >= intervalCutoff) & (lick_data['FilteredInterval'] <= 160)].mean()
+                Efficiency = len(lick_data['FilteredInterval'][(lick_data['FilteredInterval'] >= intervalCutoff) & (lick_data['FilteredInterval'] <= 160)]) / (lick_data['LickTotal'].max())
                 BurstLatency = burst_data['Start'][0]
                 DoubleContacts = lick_data['DoubleContact'].sum()
 
@@ -164,7 +200,7 @@ def Davis_Read(folder=None,
                 MPI = np.nan; Efficiency = np.nan;BurstLatency = np.nan; DoubleContacts = 0
             
             
-            # Generate Output
+            #%% Generate Output
             output = pd.DataFrame({
                 'Trial': [trialN+1],#in brackets to fool pandas into thinking it isn't all scalars. Once is enough, I guess!
                 'Solution': trial_lab,
@@ -186,7 +222,8 @@ def Davis_Read(folder=None,
                 'Animal': directory.at[fileN, 'Animal'],
                 'File': fileIs}
                 )
-# Save Output            
+            
+            #%% Save Output            
             lick_export.append(output) #Tag this trial's data onto the existing data
 
         lick_export_df = pd.concat(lick_export, ignore_index=True)
@@ -206,12 +243,26 @@ def Davis_Read(folder=None,
             output_path = os.path.join(ili_folder, f"{directory['Files'][fileN]}_ILI_export.csv")
             licks_dav.to_csv(output_path, index=False, header=0)
             print(f"Data exported to {output_path}")
+
+        returnedOutput[directory['Files'][fileN]] = lick_export_df
         
-        if timer:
-            elapsed_time = time.time() - start_time
-            print(f"Execution time: {elapsed_time:.2f} seconds")
-               
-    return lick_export_df
+        #%%Plotting
+        if plot_raw_interval:
+            all_ILI = licks_dav.values.flatten()
+            plt.hist(all_ILI, bins=50, range=(0, 500), edgecolor='black')
+            plt.xlabel('Interval (mS)')
+            plt.ylabel('Frequency')
+            plt.title('Primary Distribution of Unfiltered ILIs')
+            plt.xlim(0, 500)
+
+        
+        
+        
+    #%% Export
+    if file is None:
+        return returnedOutput
+    else:
+        return lick_export_df
 
 #%% Example usage
 #output = Davis_Read(folder="/home/ramartin/Documents/Code/Python/Davis/Test_Data", output_folder="/home/ramartin/Documents/Code/Python/Davis/Test_Result", ili_folder="/home/ramartin/Documents/Code/Python/Davis/Test_Result")
